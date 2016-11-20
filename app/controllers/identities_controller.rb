30d4d60381d177bf377fadf58216574603ca3dbc
Eben <eben.lowe@gmail.com>
2016-11-19 22:15:18 -0500

    Handle FB sign-in parameter differences from Google oauth2.

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -5,6 +5,7 @@ class IdentitiesController < ApplicationController
     if identity.present?
       sign_in(identity.user)
       remember(identity.user)
+      flash[:success] = "Signed in as #{identity.user.handle}."
       redirect_to post_auth_path
     else
       session[:auth] = request.env["omniauth.auth"].slice("info","provider","uid")
@@ -26,8 +27,13 @@ class IdentitiesController < ApplicationController
   def new
     if session[:auth].present?
       @user = User.new
-      @first_name = session[:auth]["info"]["first_name"]
-      @last_name = session[:auth]["info"]["last_name"]
+      if session[:auth]["info"]["first_name"].present?
+        @first_name = session[:auth]["info"]["first_name"]
+        @last_name = session[:auth]["info"]["last_name"]
+      else
+        @first_name = session[:auth]["info"]["name"].split(" ").first
+        @last_name = session[:auth]["info"]["name"].split(" ").last
+      end
       @times = User::REMINDER_TIMES
     else
       redirect_to sign_in_path
@@ -41,6 +47,7 @@ class IdentitiesController < ApplicationController
       session.delete(:auth)
       sign_in(user)
       remember(user)
+      flash[:success] = "Accounts linked successfully. Signed in as #{user.handle}."
       redirect_to post_auth_path
     else
       flash[:alert] = "There was an error linking your accounts."
